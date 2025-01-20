fd21ac8abb8f929b3e4d6e11f5940e79092fbac0
iambasantarai <iambasantarai@gmail.com>
2025-01-20 21:30:58 +0545

    Read values from command line flags

--- main.go
+++ main.go
@@ -1,23 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
-	todos := Todos{}
+	idPtr := flag.Int("i", 0, "id of todo")
+	titlePtr := flag.String("t", "Untitled", "title for todo")
+	descriptionPtr := flag.String("d", "", "description for todo")
 
-	todos.add("Refactor code", "My code definetly needs a proper refactoring")
-	todos.add("Learn react", "Frontend gg")
-	todos.add("Write a unit test for s3explorer", "")
-	fmt.Println("TODO: before")
-	for _, todo := range todos {
-		fmt.Printf("[%t] %s\n %s\n\n", todo.Completed, todo.Title, todo.Description)
-	}
+	flag.Parse()
 
-	fmt.Println("TODO: after")
-	todos.toggle(2)
-	todos.delete(1)
-	todos.edit(0, "Message her", "You should message her man")
-	for _, todo := range todos {
-		fmt.Printf("[%t] %s\n %s\n\n", todo.Completed, todo.Title, todo.Description)
-	}
+	fmt.Println(*idPtr)
+	fmt.Println(*titlePtr)
+	fmt.Println(*descriptionPtr)
 }
