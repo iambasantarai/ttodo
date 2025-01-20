7908116b597ce68ce716cb75b9116bf4d95253ce
iambasantarai <iambasantarai@gmail.com>
2025-01-20 22:00:27 +0545

    Read the cli args but how can i parse flags?

--- main.go
+++ main.go
@@ -3,16 +3,53 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
-	idPtr := flag.Int("i", 0, "id of todo")
-	titlePtr := flag.String("t", "Untitled", "title for todo")
-	descriptionPtr := flag.String("d", "", "description for todo")
+	cliArgs := os.Args
+	if len(cliArgs) < 2 {
+		fmt.Println("expected 'add', 'update', 'remove', or 'toggle' subcommand")
+		os.Exit(1)
+	}
 
-	flag.Parse()
+	opCommand := cliArgs[1]
 
-	fmt.Println(*idPtr)
-	fmt.Println(*titlePtr)
-	fmt.Println(*descriptionPtr)
+	cliArgs = cliArgs[2:]
+
+	switch opCommand {
+	case "add":
+		title := flag.String("t", "Untitled", "title for todo")
+		description := flag.String("d", "", "description for todo")
+
+		flag.Parse()
+
+		fmt.Println(*title)
+		fmt.Println(*description)
+	case "toggle":
+		id := flag.Int("i", 0, "id of todo")
+
+		flag.Parse()
+
+		fmt.Println(*id)
+	case "update":
+		id := flag.Int("i", 0, "id of todo")
+		title := flag.String("t", "Untitled", "title for todo")
+		descriptionPtr := flag.String("d", "", "description for todo")
+
+		flag.Parse()
+
+		fmt.Println(*id)
+		fmt.Println(*title)
+		fmt.Println(*descriptionPtr)
+	case "remove":
+		id := flag.Int("i", 0, "id of todo")
+
+		flag.Parse()
+
+		fmt.Println(*id)
+	default:
+		fmt.Printf("Unknown command: %s", cliArgs[1])
+		os.Exit(1)
+	}
 }
