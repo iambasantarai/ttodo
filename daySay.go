18a6168748bba161c157fa5b49b5ff9824ff4324
iambasantarai <iambasantarai@gmail.com>
2025-02-14 20:30:10 +0545

    Create a function to get colored messages

--- daySay.go
+++ daySay.go
@@ -11,30 +11,34 @@ func daySay() string {
 	weekDay := today.Weekday().String()
 
 	prefix := fmt.Sprintf(
-		"\033[38;5;45;1m%s \033[38;5;196;1m>\033[0m ",
+		"\033[38;5;196;1m--\033[0m \033[38;5;45;1m%s \033[38;5;196;1m--\033[0m ",
 		getAbbreviation(weekDay),
 	)
 
 	switch strings.ToLower(weekDay) {
 	case "sunday":
-		return prefix + "\033[38;5;213;1mLast day of freedom!\033[0m"
+		return getColoredMessage(prefix, "Last day of freedom!")
 	case "monday":
-		return prefix + "\033[38;5;213;1mWhy is Monday a thing?\033[0m"
+		return getColoredMessage(prefix, "Why is Monday a thing?")
 	case "tuesday":
-		return prefix + "\033[38;5;213;1mAt least it’s not Monday.\033[0m"
+		return getColoredMessage(prefix, "At least it’s not Monday.")
 	case "wednesday":
-		return prefix + "\033[38;5;213;1mHalfway to freedom!\033[0m"
+		return getColoredMessage(prefix, "Halfway to freedom!")
 	case "thursday":
-		return prefix + "\033[38;5;213;1mSo close, yet so far.\033[0m"
+		return getColoredMessage(prefix, "So close, yet so far.")
 	case "friday":
-		return prefix + "\033[38;5;213;1mWork today, party tomorrow!\033[0m"
+		return getColoredMessage(prefix, "Work today, party tomorrow!")
 	case "saturday":
-		return prefix + "\033[38;5;213;1mNo alarms, no stress!\033[0m"
+		return getColoredMessage(prefix, "No alarms, no stress!")
 	default:
-		return "\033[38;5;213;1mChill mode: ON.\033[0m"
+		return getColoredMessage(prefix, "Chill mode: ON.")
 	}
 }
 
 func getAbbreviation(weekDay string) string {
 	return string([]rune(weekDay)[:3])
+}
+
+func getColoredMessage(prefix, message string) string {
+	return prefix + "\033[38;5;213;1m" + message + "\033[0m"
 }
